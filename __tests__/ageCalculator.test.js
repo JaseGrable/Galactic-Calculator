fc3ad254caeed4f8517755f30ba0c424895e052c
JaseGrable <128560337+JaseGrable@users.noreply.github.com>
2023-06-23 12:43:20 -0700

    WIP tests for mercury age

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -6,9 +6,12 @@ describe ('User', () => {
     beforeEach(() => {
         user = new User (30);
     });
-
+// Determine user's Earth age. 
     test('should correctly create a user object with a userAge property', () => {
         expect(user.age).toEqual(30);
     });
-
+// Determine Mercury Age 
+    test('should correctly convert users age into Mercury years', ()=> {
+        expect(user.calculator("Mercury")).toEqual(125)
+    })
 });
